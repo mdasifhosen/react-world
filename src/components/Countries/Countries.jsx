b301ed61500b945e5253f6d0be8e282281a9b0d1
mdasifhosen <mdasifhosen2028@gmail.com>
2026-05-13 10:30:17 +0600

    added e country

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = () => {
-    return (
-        <div>
-            <h1>in the contry</h1>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  // console.log(countries)
+
+  return (
+    <div>
+      <h1>in the countries: {countries.length}</h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
